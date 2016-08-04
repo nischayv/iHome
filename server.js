26a9142f63d25fd57e0d55b2a71e7279ba680111
Nischay Venkatram <nischay.v@gmail.com>
2016-08-03 19:50:47 -0500

    Added new modules.  Also set up the server.

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var app = express();
+
+// Uncomment after adding favicon in images folder
+// app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
+
+var db = require('./config/db');
+// require('./app/model/Post');
+// require('./app/model/Comment');
+// require('./app/model/User');
+require('./config/passport');
+
+// set our port
+var port = process.env.PORT || 8080;
+
+mongoose.connect(db.url);
+
+// get all data/stuff of the body (POST) parameters
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public'));
+app.use(passport.initialize());
+
+// routes
+var routes = require('./app/routes');
+app.use('/', routes);
+
+//start app at localhost:8080
+app.listen(port);
+console.log('Listening on  port ' + port);
+
+exports = module.exports = app;
