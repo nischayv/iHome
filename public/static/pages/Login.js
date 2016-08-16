75314316bfc6ab963a1a348e970033f77bfd2c3c
Nischay Venkatram <nischay.v@gmail.com>
2016-08-16 01:17:05 -0500

    Need to add callbacks for the button click for each method of authentication

--- public/static/pages/Login.js
+++ public/static/pages/Login.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Button, Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 export default class Login extends React.Component {
     render() {
         return (
             <Row>
-                <a className="btn btn-block btn-social btn-lg btn-facebook">
-                    <i className="fa fa-facebook"></i>
-                    Sign in with Facebook
-                </a>
+                <Col md={6} mdOffset={3}>
+                    <a className="btn btn-block btn-social btn-lg btn-facebook">
+                        <i className="fa fa-facebook"></i>
+                        Sign in with Facebook
+                    </a>
+                    <a className="btn btn-block btn-social btn-lg btn-google">
+                        <i className="fa fa-google"></i>
+                        Sign in with Google
+                    </a>
+                    <a className="btn btn-block btn-social btn-lg btn-github">
+                        <i className="fa fa-github"></i>
+                        Sign in with Github
+                    </a>
+                </Col>
             </Row>
         );
     }
