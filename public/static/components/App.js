dce7ba1a469711190dcd67f34464a74f91447fcf
Nischay Venkatram <nischay.v@gmail.com>
2016-08-20 02:14:41 -0500

    Moved the logic for the ajax request to get the current user into the app component so that it will be available to all the sub routes

--- public/static/components/App.js
+++ public/static/components/App.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import browserHistory from "react-router";
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    getCurrentUser() {
+        $.ajax({
+            url: 'http://localhost:8080/api/isLoggedIn',
+            method: 'GET',
+            success: function(data) {
+                return data.user;
+            },
+            error: function() {
+                browserHistory.push('/login');
+            }
+        });
+    }
+
     render() {
+        const childrenWithProps = React.Children.map(this.props.children,
+            (child) => React.cloneElement(child, {
+                user: this.user
+            })
+        );
+
         return (
             <Row>
                 <Col md={6} mdOffset={3}>
-                    {this.props.children}
+                    {childrenWithProps}
                 </Col>
             </Row>
         );
--- public/static/pages/Home.js
+++ public/static/pages/Home.js
@@ -4,22 +4,6 @@ import $ from "jquery";
 import browserHistory from "react-router";
 
 export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    getCurrentUser() {
-        $.ajax({
-            url: 'http://localhost:8080/api/isLoggedIn',
-            method: 'GET',
-            success: function(data) {
-                return data.user;
-            },
-            error: function() {
-                browserHistory.push('/login');
-            }
-        });
-    }
 
     render() {
         return (
